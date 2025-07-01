91ee0e7846b3016b336b19928615430e76a10b6c
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-01 15:57:07 +0200

    rendu conditionnel

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 const nom: string = "Chadrac";
+const age: number = 12;
 const Welcome = () => {
   return (
     <>
       <h2>Bienvenue dans mon application, cher {nom === "Chadrac" ? "Invité" : "Inconnu"}</h2>
+      <p>{ age < 18 ? 'Tu es mineur' : 'Tu es majeur'}</p>
     </>
   );
 };
+
 const Test = () => {  
-  return <h2>Nous sommes de retour</h2>
+  return (<h2>Nous sommes de retour</h2>);
 }
 
 createRoot(document.getElementById('root')!).render(
