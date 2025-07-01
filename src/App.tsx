f4f2ac151ddd3c823fc72b3933cae7f574fed898
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-01 16:27:26 +0200

    props notions amorced

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
-    <h1>Je suis le composant principal</h1>
+    <>
+      <h1>Je suis le composant principal</h1>
+      <Salutation name="Chadrac" />
+      <Salutation name="Wakomya" />
+      <Salutation name="Kighundila" />
+    </>
+    
   );
 }
 
 export default App
+
+type SalutationProps = {
+  name: string;
+}
+const Salutation = (props: SalutationProps) => {
+  return (
+    <h2>Bonjour, {props.name}</h2>
+  );
+}
