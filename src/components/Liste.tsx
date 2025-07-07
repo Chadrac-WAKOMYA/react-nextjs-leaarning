a4cf61ad4427a7f5c0a40f7d89bab60535595e10
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-07 04:52:37 +0200

    monday ready to continue

--- src/components/Liste.tsx
+++ src/components/Liste.tsx
@@ -7,14 +7,14 @@ export default class Liste extends PureComponent<Props> {
     const users = [
           { id: 1, name: 'Chadrac', age: 16 },
           { id: 2, name: 'Wakomya', age: 20 },
-        //   { id: 3, name: 'Kighundila', age: 18 },
+          { id: 3, name: 'Kighundila', age: 18 },
         ];
     return (
       <>
         {
             users.map((user) => (
                 <div style={{padding: '10px', border: '1px solid black', margin: '5px'}} key={user.id}>
-                    {/* <p>{user.name}---{user.age}</p> */}
+                    <p>{user.name}---{user.age}</p>
                 </div>
             ))
         }
