7476b474c73acaf114f43bfe9637f9842ddac5c9
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-07 05:34:23 +0200

    Compteur component initialised

--- src/components/Compteur.tsx
+++ src/components/Compteur.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 type Props = {}
 
 export default function Compteur({}: Props) {
-  return (
-    <div>Les événements en React</div>
-  )
+    const handleClick = () => {
+        console.log('Incrémentation du compteur');
+    }
+    return (
+        <div>
+            <h1>Les événements en React</h1>
+            <h3>Nombre de fois : 0</h3>
+            <button onClick={handleClick}>Incrémenter</button>
+        </div>
+        
+    )
 }
